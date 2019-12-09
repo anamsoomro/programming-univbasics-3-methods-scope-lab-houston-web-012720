fce746a7a40a11c56d9c54d5f7f5ed9e7d8145eb
anam soomro <anamsoomroed@gmail.com>
2019-12-09 05:04:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,9 +10,9 @@ def toadstool
 end
 
 def link
-  put "It's Dangerous To Go Alone! Take This."
+  puts "It's Dangerous To Go Alone! Take This."
 end
 
 def any_phrase (phrase)
-  put phrase
+  puts phrase
 end
